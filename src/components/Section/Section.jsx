c328c23163dd68b1f70c7d2a728cfb4be66a2c5a
Agustín Lanusse <lanusseagustin@gmail.com>
2021-06-22 23:11:27 -0300

    added desktop support ti section component

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -2,15 +2,20 @@ import React from 'react'
 import TextBlock from '../TextBlock/TextBlock'
 import './Section.css'
 
-function Section({ title, image, textBlocks, highlight, backgroundPattern }) {
+function Section({ title, image, textBlocks, highlight }) {
   return (
     <section className={`section ${highlight && 'section--highlight'}`}>
       {title && <h2 className='section__title'>{title}</h2>}
       <div
-        className='section__contentContainer'>
+        className={`section__contentContainer ${image.imagePosition === 'right' && 'section__contentContainer--reverse'}`}>
         <img
-          className='section__image'
+          className='section__image section__image--mobile'
           src={process.env.PUBLIC_URL + image.mobile}
+          alt={title || textBlocks[0].title}
+        />
+        <img
+          className='section__image section__image--desktop'
+          src={process.env.PUBLIC_URL + image.desktop}
           alt={title || textBlocks[0].title}
         />
         <div className='section__textsContainer'>
